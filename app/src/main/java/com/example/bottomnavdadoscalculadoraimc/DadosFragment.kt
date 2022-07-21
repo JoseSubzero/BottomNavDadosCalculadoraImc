8ce828d68f66a4cc9c52f34c4d93ba52ce059638
Alumno <fdsf>
2022-07-21 20:16:04 +0200

    Commit segundo. Dados completados

--- app/src/main/java/com/example/bottomnavdadoscalculadoraimc/DadosFragment.kt
+++ app/src/main/java/com/example/bottomnavdadoscalculadoraimc/DadosFragment.kt
@@ -6,11 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.bottomnavdadoscalculadoraimc.databinding.FragmentDadosBinding
+import kotlin.random.Random
 
 
 class DadosFragment : Fragment() {
     private var _binding: FragmentDadosBinding? = null
     private val binding get() = _binding!!
+    private val dados = arrayOf(R.drawable.dice1, //Son enteros basados en su indentificaor como si fueran un codigo de barras/etiqueta
+        R.drawable.dice2,
+        R.drawable.dice3,
+        R.drawable.dice4,
+        R.drawable.dice5,
+        R.drawable.dice6)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,5 +34,24 @@ class DadosFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.ivDado1.setOnClickListener {
+            val d1 = Random.nextInt(1, 7)
+            val d2 = Random.nextInt(1, 7)
+            binding.ivDado1.setImageResource(dados[d1-1])
+            binding.ivDado2.setImageResource(dados[d2-1])
+            binding.tvResultado.text = (d1 + d2).toString()
+        }
+        binding.ivDado2.setOnClickListener {
+            val d1 = Random.nextInt(1, 7)
+            val d2 = Random.nextInt(1, 7)
+            binding.ivDado1.setImageResource(dados[d1-1])
+            binding.ivDado2.setImageResource(dados[d2-1])
+            binding.tvResultado.text = (d1 + d2).toString()
+        }
+
+
+
     }
+
 }
